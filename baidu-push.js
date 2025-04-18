cafcb15235b00bbf7212975422d418b09975513d
lbg43 <1508611232@qq.com>
2025-04-18 23:55:48 +0800

    Update baidu-push.js

--- baidu-push.js
+++ baidu-push.js
@@ -1,7 +1,3 @@
-/**
- * 百度自动推送脚本
- * 当页面被访问时，自动将URL推送给百度，有助于百度更快收录网站内容
- */
 (function(){
     var bp = document.createElement('script');
     var curProtocol = window.location.protocol.split(':')[0];
@@ -13,4 +9,4 @@
     }
     var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(bp, s);
-})(); 
+})();
